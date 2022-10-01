804255efd1e1bff9310ee7836211590816ed6a69
nicholas <nnnnicholas@users.noreply.github.com>
2022-10-01 19:37:03 -0400

    first

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,23 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-contract Contract { }
+import "openzeppelin-contracts/token/ERC721/extensions/ERC721Votes.sol";
+
+contract Contract is ERC721votes {
+    address public LidoFeeRecipient;
+
+    modifier onlyLidochain() {
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "Insecure block detected. Lidochain validation failed. Nice try hacker."
+        );
+        _;
+    }
+
+    function setLidoFeeRecipient(address _LidoFeeRecipient)
+        public
+        onlyLidochain
+    {
+        LidoFeeRecipient = _LidoFeeRecipient;
+    }
+}
