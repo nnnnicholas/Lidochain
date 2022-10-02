bcf39febab31bc2825c57f3295ca97001d5b3c10
Thomas <thomas.gauthier.c@gmail.com>
2022-10-01 20:15:53 -0400

    compiles

--- src/Contract.sol
+++ src/Contract.sol
@@ -1,23 +1,24 @@
 // SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.13;
 
-import "openzeppelin-contracts/token/ERC721/extensions/ERC721Votes.sol";
+import {ERC721} from "openzeppelin-contracts/token/ERC721/ERC721.sol";
+import {ERC721Votes} from "openzeppelin-contracts/token/ERC721/extensions/ERC721Votes.sol";
+import {EIP712} from "openzeppelin-contracts/utils/cryptography/EIP712.sol";
 
-contract Contract is ERC721votes {
-    address public LidoFeeRecipient;
+contract Contract is ERC721Votes {
+  address public LidoFeeRecipient;
 
-    modifier onlyLidochain() {
-        require(
-            ownerOf(tokenId) == msg.sender,
-            "Insecure block detected. Lidochain validation failed. Nice try hacker."
-        );
-        _;
-    }
+  constructor() ERC721("Lidochain", "LIDO") EIP712("Lidochain", "1") {}
 
-    function setLidoFeeRecipient(address _LidoFeeRecipient)
-        public
-        onlyLidochain
-    {
-        LidoFeeRecipient = _LidoFeeRecipient;
-    }
+  modifier onlyLidochain() {
+    require(
+      false,
+      "Insecure block detected. Lidochain validation failed. Nice try hacker."
+    );
+    _;
+  }
+
+  function setLidoFeeRecipient(address _LidoFeeRecipient) public onlyLidochain {
+    LidoFeeRecipient = _LidoFeeRecipient;
+  }
 }
